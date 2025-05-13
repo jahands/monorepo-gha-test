5f29f56330542b275fee46335f6c805083299d57
Jacob Hands <jacob@jacobhands.com>
2025-05-13 17:39:45 -0500

    chore: simplify script

---
+++ .changeset/rude-rocks-lay.md
@@ -0,0 +1,11 @@
+---
+'example-worker-echoback': patch
+'@repo/eslint-config': patch
+'@repo/hono-helpers': patch
+'@repo/tools': patch
+'@repo/typescript-config': patch
+'@repo/workspace-dependencies': patch
+'@repo/zod': patch
+---
+
+chore: simplify script
--- tmp.sh
+++ tmp.sh
@@ -2,9 +2,6 @@
 
 GITHUB_OUTPUT=./foo.txt
 
-{
-	echo "turbo_filters<<EOF"
-	echo '[{"name":"example-worker-echoback","version":"0.2.6"},{"name":"my-worker","version":"0.2.6"},{"name":"@repo/eslint-config","version":"0.1.3"},{"name":"@repo/hono-helpers","version":"0.1.3"}]' |
-		jq -r '[.[] | "-F \(.name)"] | join(" ")'
-	echo "EOF"
-} >>$GITHUB_OUTPUT
+echo "turbo_filters=$(
+	jq -r '[.[] | "-F \(.name)"] | join(" ")' <<<'[{"name":"example-worker-echoback","version":"0.2.6"},{"name":"my-worker","version":"0.2.6"},{"name":"@repo/eslint-config","version":"0.1.3"},{"name":"@repo/hono-helpers","version":"0.1.3"}]'
+)" >>$GITHUB_OUTPUT

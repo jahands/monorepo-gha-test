42cc7a482cc2c4c372c9474f0a5877bdea9ae310
Jacob Hands <jacob@jacobhands.com>
2025-05-13 20:08:16 -0500

    disable color in chalk

--- packages/tools/src/test/setup.ts
+++ packages/tools/src/test/setup.ts
@@ -1,4 +1,7 @@
+// Ensure chalk doesn't add colors to output for consistent snapshots
+delete process.env.FORCE_COLOR
+
 // runx uses zx/globals imported in bin/runx.ts
 // This import ensures that tests work without
 // needing to import this manually.
-import 'zx/globals'
+await import('zx/globals')
